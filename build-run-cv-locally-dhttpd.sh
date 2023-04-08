487af76692fb46cdf2d2effba49cc0fa74d8851b
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-04-08 17:53:45 +0700

    new builds going into root folder for github-pages

---
+++ build-run-cv-locally-dhttpd.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+pwd
+cd cv_flutter
+pwd
+PORTDHTTPD='1996'
+## DSGVO Build flutter app
+flutter build web \
+  --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ \
+  --web-renderer canvaskit \
+  --profile \
+  --output ..
+## Output into the root folder of the project
+
+
+cd ..
+open http://localhost:$PORTDHTTPD
+dart pub global run dhttpd --path . --port $PORTDHTTPD
+#dart pub global run dhttpd --path cv_flutter/build/web --port $PORTDHTTPD
--- run-cv-locally-dhttpd.sh
+++ run-cv-locally-dhttpd.sh
@@ -3,14 +3,17 @@
 pwd
 cd cv_flutter
 pwd
-PORTDHTTPD='1994'
+PORTDHTTPD='1997'
 ## DSGVO Build flutter app
-flutter build web \
-  --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ \
-  --web-renderer canvaskit \
-  --profile
+#flutter build web \
+#  --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ \
+#  --web-renderer canvaskit \
+#  --profile \
+#  --output ..
+## Output into the root folder of the project
 
 
 cd ..
 open http://localhost:$PORTDHTTPD
-dart pub global run dhttpd --path cv_flutter/build/web --port $PORTDHTTPD
+dart pub global run dhttpd --path . --port $PORTDHTTPD
+#dart pub global run dhttpd --path cv_flutter/build/web --port $PORTDHTTPD
