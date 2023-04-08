4252bdc8c4d66d4076ed57b808031ec43d1c7d24
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-04-08 18:29:34 +0700

    added by script load-new-web-into-github-override-old.sh

--- cv_flutter/lib/main.dart
+++ cv_flutter/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/utils.dart';
 import 'package:myapp/page-1/cv-dominik.dart' as cvDominik;
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+void main() {
+	GoogleFonts.config.allowRuntimeFetching = false;
+	runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
 	@override
--- load-new-web-into-github-no-copy.sh
+++ load-new-web-into-github-no-copy.sh
@@ -8,11 +8,11 @@ pwd
 flutter build web \
   --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ \
   --web-renderer canvaskit \
-  --release
-## DSGVO Build flutter app
-
-
-#git add .
-#git commit -m 'added by script load-new-web-into-github-override-old.sh'
-#git push
+  --profile \
+  --output ..
+## Output into the root folder of the project
+cd ..
+git add .
+git commit -m 'added by script load-new-web-into-github-override-old.sh'
+git push
 
