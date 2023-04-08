aae02568e86da84a4a93ceef4f8c0a17e2a53131
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-04-08 23:43:15 +0700

    changed web-renderer to html, thereby not downloading gstatic! dsgvo compliant

--- load-new-web-into-github-no-copy.sh
+++ load-new-web-into-github-no-copy.sh
@@ -6,14 +6,19 @@ pwd
 
 ## DSGVO Build flutter app
 flutter build web \
-  --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ \
-  --web-renderer canvaskit \
+  --web-renderer html \
   --profile \
   --output ..
 ## Output into the root folder of the project
 cd ..
 git add .
 #git commit -m 'added by script z-old-load-new-web-into-github-override-old.sh'
-git commit -m 'downloaded fonts, but gstatic is not removable: https://github.com/flutter/flutter/issues/85793 '
+git commit -m 'changed web-renderer to html, thereby not downloading gstatic! dsgvo compliant '
 git push
 
+#  --web-renderer canvaskit \
+#flutter build web \
+#  --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/ \
+#  --web-renderer canvaskit \
+#  --profile \
+#  --output ..
